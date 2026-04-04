644c6c4b971f550292f9738d8628c98f3e26dd9f
Anthony Galeano <ahorasimultiservicio360@gmail.com>
2026-04-04 01:27:20 -0700

    fix: whitelist CloudFront domain for images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'd2xsxph8kpxj0f.cloudfront.net' },
+    ],
   },
 };
 module.exports = nextConfig;
